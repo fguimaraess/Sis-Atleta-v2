b5bb5f6221cde8663c875acf2da0d13fb415e1f5
luizcunha3 <luiz.cunha.3@gmail.com>
2017-02-11 20:28:41 -0200

    Cadastro e login pelo firebase completos

--- public/js/register.js
+++ public/js/register.js
@@ -1,12 +1,66 @@
 var page = {
+  nameField: document.querySelector('#name-field'),
+  emailField: document.querySelector('#email-field'),
+  cidadeField: document.querySelector('#cidade-field'),
+  estadoField: document.querySelector('#estado-field'),
+  senhaField: document.querySelector('#password-field'),
+  senhaCheckField: document.querySelector('#verify-pwd-field'),
+  registrarBtn: document.querySelector('#cadastrar-btn'),
   voltarBtn: document.querySelector('#voltar-btn')
 }
 
-page.voltarBtn.addEventListener('click', function () {
+function checkSenha(){
+  return page.senhaField.value == page.senhaCheckField.value;
+}
+
+function saveUserDetails(usuario){
+  console.log('nome: ', page.nameField.value);
+  console.log('cidade: ', page.cidadeField.value)
+  console.log('estado: ', page.estadoField.selectedIndex);;
+  firebase.database().ref('users/' + usuario.uid).set({
+    username: page.nameField.value,
+    email: usuario.email,
+    cidade: page.cidadeField.value,
+    estado: page.estadoField.value
+  });
+}
+
+
+
+function handleCadstroErro(error){
+  // Handle Errors here.
+  var errorCode = error.code;
+  var errorMessage = error.message;
+  if (errorCode == 'auth/weak-password') {
+    alert('The password is too weak.');
+  } else {
+    alert(errorMessage);
+  }
+  console.log(error);
+}
+
+function registraUsuario() {
+  if(checkSenha()){
+    firebase.auth().createUserWithEmailAndPassword(page.emailField.value, page.senhaField.value)
+      .then(function(user){
+        saveUserDetails(user);
+      })
+      .catch(function(error) {
+        handleCadstroErro(error);
+      });
+  } else {
+    console.log('senhas diferentes');
+  }
+
+}
+
+page.voltarBtn.addEventListener('click', function() {
   window.location = '/';
 });
 
+page.registrarBtn.addEventListener('click', registraUsuario);
+
 
 $(document).ready(function() {
-   $('select').material_select();
- });
+  $('select').material_select();
+});
