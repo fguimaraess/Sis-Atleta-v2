d37ac83100a5973ea474bfadef4268774f0043a9
luizcunha3 <luizcunha@MacBook-Pro-de-Luiz.local>
2017-02-11 14:57:37 -0200

    Logica de banco de dados

---
+++ public/js/firebase.js
@@ -0,0 +1,9 @@
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyB7mrUbY-JV7YqDGwrd1rNaLW7v4c7eK90",
+  authDomain: "sis-atleta-4a38f.firebaseapp.com",
+  databaseURL: "https://sis-atleta-4a38f.firebaseio.com",
+  storageBucket: "sis-atleta-4a38f.appspot.com",
+  messagingSenderId: "661403184654"
+};
+firebase.initializeApp(config);
--- public/js/index.js
+++ public/js/index.js
@@ -1,8 +1,34 @@
 var page = {
   loginBtn: document.querySelector('#login-btn'),
-  registerBtn: document.querySelector('#register-btn')
+  registerBtn: document.querySelector('#register-btn'),
+  database: firebase.database()
 }
 
-page.registerBtn.addEventListener('click', function () {
+page.loginBtn.addEventListener('click', function() {
+
+  console.log(getUsuario('luizcunha3@gmail.com'));
+
+});
+
+page.registerBtn.addEventListener('click', function() {
   window.location = "/register.html";
 });
+
+function getUsuario(email) {
+
+  console.log('entrei no get usuarios');
+
+  page.database.ref('/usuarios').once('value').then(function(snapshot) {
+    var usuarioDatabase = false;
+    snapshot.val().forEach(function(usuario) {
+
+      if (usuario.email == email) {
+        usuarioDatabase = usuario;
+      }
+
+    });
+    return usuarioDatabase;
+
+  })
+
+}
