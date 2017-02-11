af4f707727e4f1b923a672b59399563415247849
luizcunha3 <luiz.cunha.3@gmail.com>
2017-02-11 19:32:52 -0200

    Alteraçao da avaliação de login
    Alterei o metodo de login.
    Mudei os ids dos input fields no card de login

--- public/js/index.js
+++ public/js/index.js
@@ -1,13 +1,30 @@
 var page = {
   loginBtn: document.querySelector('#login-btn'),
   registerBtn: document.querySelector('#register-btn'),
+  emailField: document.querySelector('#email-field'),
+  passwordField: document.querySelector('#password-field'),
   database: firebase.database()
 }
 
 page.loginBtn.addEventListener('click', function() {
 
-  console.log(getUsuario('luizcunha3@gmail.com'));
 
+  var user = firebase.auth().signInWithEmailAndPassword(page.emailField.value, page.passwordField.value)
+    .catch(function(error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      if (errorCode === 'auth/wrong-password') {
+        alert('Wrong password.');
+      } else {
+        alert(errorMessage);
+      }
+      console.log(error);
+    });
+
+    var user = firebase.auth().currentUser;
+
+    alert(user.email);
 });
 
 page.registerBtn.addEventListener('click', function() {
