bdfc0504a2fbb13cd914424ac856ee74982bee0c
luizcunha3 <luiz.cunha.3@gmail.com>
2017-02-15 21:54:20 -0200

    Só dei um beautify no codigo

--- public/js/atleta.js
+++ public/js/atleta.js
@@ -1,71 +1,71 @@
 var page = {
-    database: firebase.database()
-    , nomeAtleta: "Felipe"
-    , sobrenomeAtleta: "Guimarães"
-    , posicao: "Atacante"
-    , idade: 24
-    , categoria: "Profissional"
-    , clube: ""
-    , cidade: "São Gonçalo"
-    , pais: "Brasil"
-    , foto: "url('campo-futebol.jpg')"
-    , atletaBtn: document.querySelector('#novo-atleta')
+  database: firebase.database(),
+  nomeAtleta: "Felipe",
+  sobrenomeAtleta: "Guimarães",
+  posicao: "Atacante",
+  idade: 24,
+  categoria: "Profissional",
+  clube: "",
+  cidade: "São Gonçalo",
+  pais: "Brasil",
+  foto: "url('campo-futebol.jpg')",
+  atletaBtn: document.querySelector('#novo-atleta')
 }
 
 function getAtletas() {
-    page.database.ref('/atletas/').once('value').then(function (snapshot) {
-        snapshot.val().forEach(function () {
-            console.log(page.nomeAtleta);
-        });
-    })
+  page.database.ref('/atletas/').once('value').then(function(snapshot) {
+    snapshot.val().forEach(function() {
+      console.log(page.nomeAtleta);
+    });
+  })
 }
 
 function getAtletasById(idAtleta) {
-    return page.database.ref('/atletas/').once('value').then(function (snapshot) {
-        snapshot.val().idAtleta;
-        })
+  return page.database.ref('/atletas/').once('value').then(function(snapshot) {
+    snapshot.val().idAtleta;
+  })
 }
 
 function getAtletasByNome(nomeAtleta) {
-    page.database.ref('/atletas/').once('value').then(function(snapshot) {
-        snapshot.val().forEach(function (nomeAtleta) {
-        
+  page.database.ref('/atletas/').once('value').then(function(snapshot) {
+    snapshot.val().forEach(function(nomeAtleta) {
+
     });
-    })
+  })
 }
 
 function novoAtleta() {
-    page.database.ref('atletas/').push().set({
-        nomeAtleta: "Felipe"
-        , sobrenomeAtleta: "Guimarães"
-        , posicao: "Atacante"
-        , idade: 24
-        , categoria: "Profissional"
-        , clube: ""
-        , cidade: "São Gonçalo"
-        , pais: "Brasil"
-        , foto: "url('campo-futebol.jpg')"
-    });
+  page.database.ref('atletas/').push().set({
+    nomeAtleta: "Felipe",
+    sobrenomeAtleta: "Guimarães",
+    posicao: "Atacante",
+    idade: 24,
+    categoria: "Profissional",
+    clube: "",
+    cidade: "São Gonçalo",
+    pais: "Brasil",
+    foto: "url('campo-futebol.jpg')"
+  });
 }
 
 function editarAtleta( /*idAtleta*/ ) {
-    var atleta = {
-        nomeAtleta: "Marquinho"
-        , sobrenomeAtleta: "Calazans"
-        , posicao: "Atacante"
-        , idade: 20
-        , categoria: "Profissional"
-        , clube: ""
-        , cidade: "São Gonçalo"
-        , pais: "Brasil"
-        , foto: "url('campo-futebol.jpg')"
-    }
-    var updates = {};
-    updates['/atletas/' + /*idAtleta*/ ] = atleta;
-    return page.database.ref().update(updates);
+  var atleta = {
+    nomeAtleta: "Marquinho",
+    sobrenomeAtleta: "Calazans",
+    posicao: "Atacante",
+    idade: 20,
+    categoria: "Profissional",
+    clube: "",
+    cidade: "São Gonçalo",
+    pais: "Brasil",
+    foto: "url('campo-futebol.jpg')"
+  }
+  var updates = {};
+  updates['/atletas/' + /*idAtleta*/ ] = atleta;
+  return page.database.ref().update(updates);
 }
 
 function excluirAtleta( /*idAtleta*/ ) {
-    page.database.ref('atletas/' + /*idAtleta*/ ).remove();
+  page.database.ref('atletas/' + /*idAtleta*/ ).remove();
 }
 page.atletaBtn.addEventListener('click', novo);
