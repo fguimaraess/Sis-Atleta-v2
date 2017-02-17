6a9af7a0dad62ba8b7ddf9cd8f83c0f80e6f677b
luizcunha3 <luiz.cunha.3@gmail.com>
2017-02-16 22:44:51 -0200

    Alteraçoes no js de atleta

--- public/js/atleta.js
+++ public/js/atleta.js
@@ -1,8 +1,8 @@
 var page = {
   database: firebase.database(),
   databaseRef: 'atletas/',
-  linhasAtleta: //peguei la o id linha atleta
-  templateLinha: //mandinga pra pegar o templatelinha
+  linhasAtleta: 'a', //VER C LUIZ
+  templateLinha: 'b', //VER C LUIZ
   nomeField: "Felipe",
   sobrenomeField: "Guimarães",
   posicaoField: "Atacante",
@@ -15,35 +15,13 @@ var page = {
   atletaBtn: document.querySelector('#novo-atleta')
 }
 
-function novoAtleta(atleta){
-    page.database.ref('/atletas/' + atleta.atletaId).set(atleta);
+function novoAtleta(atleta) {
+  page.database.ref('/atletas/').push(atleta);
 }
 
 //OK
-function criaAtleta(){
-var myId = page.database.ref('/atletas/').push();
-var newId = myId.key;
+function criaAtleta() {
   var atleta = {
-  atletaId: newId,
-  nome: page.nomeAtleta,
-  sobrenome: page.sobrenomeField,
-  posicao: page.posicaoField,
-  idade: page.idadeField,
-  categoria: page.categoriaField,
-  clube: page.clubeField,
-  cidade: page.cidadeField,
-  pais: page.paisField,
-  foto: page.fotoField,
-  }
-  console.log("ID GERADO: " + newId);
-  novoAtleta(atleta);
-}
-
-page.atletaBtn.addEventListener('click', criaAtleta);
-
-//Testando o getAtletasByNome
-var testeGetByNome = {
-    atletaId: "-Kd6-Riwd6LgtLDY_xpK",
     nome: page.nomeAtleta,
     sobrenome: page.sobrenomeField,
     posicao: page.posicaoField,
@@ -52,26 +30,44 @@ var testeGetByNome = {
     clube: page.clubeField,
     cidade: page.cidadeField,
     pais: page.paisField,
-    foto: page.fotoField
+    foto: page.fotoField,
+  }
+  novoAtleta(atleta);
 }
+
+page.atletaBtn.addEventListener('click', function () {
+  var atleta = {nome: "Luiz"};
+  getAtletas();
+});
+
 //OK
-function getAtletasByNome(atleta) {
-  page.database.ref(page.databaseRef + atleta.atletaId).once('value').then(function(snapshot) {
-    snapshot.forEach(function(atleta) {
-        console.log(atleta.val());
+function getAtletasByNome(mozao) {
+  var atletas = [];
+  page.database.ref(page.databaseRef).once('value').then(function(snapshot) {
+    snapshot.forEach(function(atletaRef) {
+      if(mozao.nome == atletaRef.val().nome){
+
+        var tempAtleta = atletaRef.val();
+        tempAtleta.uid = atletaRef.key;
+        atletas.push(tempAtleta);
+
+      }
     });
+     console.log(atletas);
   });
 }
 
 //OK
-function getAtletas(){
-    page.database.ref(page.databaseRef).once('value').then(function(snapshot){
-        snapshot.forEach(function (atleta){
-            var id = atleta.val().atletaId;
-            console.log("ESSE É O ID do Atleta " + atleta.val().nome + ": " + id);
-            console.log(atleta.val());
-        });
+function getAtletas() {
+  var atletas = [];
+  page.database.ref(page.databaseRef).once('value').then(function(snapshot) {
+    snapshot.forEach(function(atleta) {
+      var tempAtleta = atleta.val();
+      tempAtleta.uid = atleta.key;
+      atletas.push(tempAtleta);
     });
+    console.log(atletas);
+  });
 }
 
 
@@ -80,7 +76,7 @@ function preencheTabelaAtletas() {
   var atletas = getAtletas();
   linhasAtleta.innerHTML = "";
   var linhaNova;
-  atletas.forEach(function (atleta) {
+  atletas.forEach(function(atleta) {
     linhaNova = templateLinha.cloneNode()
     linhaNova.removeHidden;
     linhaNova.querySelector('.nome-atle....').text = atleta.nome;
@@ -125,9 +121,9 @@ function salvaModal() {
 function salvaAlteracoes(atleta) {
   atleta.nome = page.nomeField.textContent;
   atleta.sobrenome = page.sobrenomeField.textContent;
-  .
-  .
-  .
+  // .
+  // .
+  // .
   editarAtleta(atleta);
 
 }
@@ -139,8 +135,8 @@ function editarAtleta(atleta) {
 function excluirAtleta(atleta) {
   page.database.ref('atletas/' + atleta.id).remove();
 }
-page.atletaBtn.addEventListener('click', getAtletasByNome);
 
-page.abreListaAtleta(){
 
-}
+// page.abreListaAtleta() {
+//
+// }
