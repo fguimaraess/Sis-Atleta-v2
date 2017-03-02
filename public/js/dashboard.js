eeff206d54163cd1cde3f7e1b2a4d32be482c983
fguimaraess <felipeemg@gmail.com>
2017-03-02 11:16:03 -0300

    Tentativa de Toggle nos menus

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,11 +1,24 @@
 var pageBtn = {
     atletasSideBtn: document.querySelector('#atletas-menu'),
+    clubesSideBtn: document.querySelector('#clubes-menu'),
     relatoriosSideBtn: document.querySelector('#relatorios-menu')
 }
 
 pageBtn.relatoriosSideBtn.addEventListener('click', function(){
   swal("Ops...", "Menu de Relatórios em desenvolvimento");
 })
+
+$(document).ready(function(){
+    $(pageBtn.atletasSideBtn).click(function(){
+        $('#view-atletas').toggle();
+    });
+});
+
+$(document).ready(function(){
+    $('#clubes-menu').click(function(){
+        $('#view-clubes').toggle();
+    });
+});
 
 $(document).ready(function(){
     // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
