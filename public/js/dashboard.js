b1364f2f0790d451117d45855f351ba10aebcc8b
Felipe Guimarães <felipeemg@gmail.com>
2017-04-18 22:04:46 -0300

    Traz estatisticas do clube no relatorio

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -55,6 +55,7 @@ $(document).ready(function () {
     $('select').material_select();
 
     $('.datepicker').pickadate({
+        format: 'dd/mm/yyyy',
         selectMonths: true, // Creates a dropdown to control month
         selectYears: 15, // Creates a dropdown of 15 years to control year
         today: 'Hoje',
--- public/js/relatorio.js
+++ public/js/relatorio.js
@@ -1,4 +1,9 @@
 var pageRelatorio = {
+    golsPro: 0,
+    golsContra: 0,
+    vitorias: 0,
+    derrotas: 0,
+    empates: 0,
     reportAtleta: document.querySelector('#report-atleta')
     , reportClube: document.querySelector('#report-clube')
     , atletaField: document.querySelector('#report-atleta-combo')
@@ -7,20 +12,53 @@ var pageRelatorio = {
     , dataFimField: document.querySelector('#data-fim-field')
     , searchBtn: document.querySelector('#search-btn')
     , bodyDadosClubes: document.querySelector('#body-dados-clube')
-    , limparBtn: document.querySelector('#limpar-relatorio')
+    , limparBtn: document.querySelector('#limpar-relatorio'),
+    estatisticasClube: document.querySelector('#estatisticas-clube'),
+    tableEstatisticasClube: document.querySelector('#table-estatistica-clube')
 }
 pageRelatorio.reportAtleta.addEventListener('click', function () {
+    $(pageRelatorio.tableEstatisticasClube).hide();
     getClubesReport();
     getJogos();
 })
 pageRelatorio.reportClube.addEventListener('click', function () {
+    $(pageRelatorio.tableEstatisticasClube).hide();
     getClubesReport();
     getJogos();
 })
 pageRelatorio.searchBtn.addEventListener('click', function () {
+    $(pageRelatorio.tableEstatisticasClube).show();
+    pageRelatorio.golsPro = 0;
+        pageRelatorio.golsContra = 0;
+        pageRelatorio.vitorias = 0;
+        pageRelatorio.derrotas = 0;
+        pageRelatorio.empates = 0;
+    
     var clubeSelecionado = $(pageRelatorio.clubeField).val();
     getDadosClube(clubeSelecionado);
+    var tempDados = {
+        golsPro: pageRelatorio.golsPro,
+        golsContra: pageRelatorio.golsContra,
+        vitorias: pageRelatorio.vitorias,
+        derrotas: pageRelatorio.derrotas,
+        empates: pageRelatorio.empates
+    }
+    getEstatisticasClube(tempDados);
 })
+
+function getEstatisticasClube(tempDados)
+{
+    var qtdJogos = tempDados.vitorias + tempDados.derrotas + tempDados.empates;
+    var saldoDeGols = tempDados.golsPro - tempDados.golsContra;
+    var htmlDados = '<td>' + qtdJogos + '</td>';
+    htmlDados += '<td>' + tempDados.vitorias + '</td>';
+    htmlDados += '<td>' + tempDados.empates + '</td>';
+    htmlDados += '<td>' + tempDados.derrotas + '</td>';
+    htmlDados += '<td>' + tempDados.golsPro + '</td>';
+    htmlDados += '<td>' + tempDados.golsContra + '</td>';
+    htmlDados += '<td>' + saldoDeGols + '</td><p>';
+    pageRelatorio.estatisticasClube.innerHTML = htmlDados;
+}
 
 function getDadosClube(tempClube) {
     var jogosNaTela = document.querySelectorAll('.idDosJogosFiltrados');
@@ -30,10 +68,11 @@ function getDadosClube(tempClube) {
     tempJogosClube = pageJogo.jogos;
     var dadosClube = [];
     var trocaDados = [];
-    dadosClube.totalGols = 0;
     for (var key in tempJogosClube) {
         if (tempClube == tempJogosClube[key].meuclube) {
             dadosClube = tempJogosClube[key];
+            dadosClube.golsPro = tempJogosClube[key].golsmeuclube;
+            dadosClube.golsContra = tempJogosClube[key].golsclubeadversario;
             preencheReportClube(dadosClube);
         }
         if (tempClube == tempJogosClube[key].clubeadversario) {
@@ -44,6 +83,8 @@ function getDadosClube(tempClube) {
             trocaDados.local = tempJogosClube[key].local;
             trocaDados.data = tempJogosClube[key].data;
             trocaDados.uid = tempJogosClube[key].uid;
+            trocaDados.golsPro = trocaDados.golsmeuclube;
+            trocaDados.golsContra = trocaDados.golsclubeadversario;
             preencheReportClube(trocaDados);
         }
     }
@@ -51,6 +92,16 @@ function getDadosClube(tempClube) {
 
 function preencheReportClube(tempJogosClube) {
     var dadosClube = tempJogosClube;
+    pageRelatorio.golsPro += parseInt(tempJogosClube.golsPro);
+    pageRelatorio.golsContra += parseInt(tempJogosClube.golsContra);
+    
+    if(tempJogosClube.golsmeuclube > tempJogosClube.golsclubeadversario){
+        pageRelatorio.vitorias++;
+    } else if(tempJogosClube.golsmeuclube < tempJogosClube.golsclubeadversario){
+        pageRelatorio.derrotas++;
+    } else {
+        pageRelatorio.empates++;
+    }
     var html = '';
     html += '<tr  class="idDosJogosFiltrados" id="' + dadosClube.uid + '">';
     html += '<td class="meuClube">' + dadosClube.meuclube + '</a></td>';
@@ -87,4 +138,7 @@ pageRelatorio.limparBtn.addEventListener('click', function () {
     jogosNaTela.forEach(function (jogosHtml) {
         pageRelatorio.bodyDadosClubes.innerHTML = '';
     });
+    pageRelatorio.dataInicioField.value = '';
+    pageRelatorio.dataFimField.value = '';
+    $(pageRelatorio.tableEstatisticasClube).hide();
 })
